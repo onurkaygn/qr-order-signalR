2f370593af05bac4bfc0a4420f9b066dcb75cdba
= <onuralpkayginedu@gmail.com>
2024-09-27 15:38:42 +0300

    Created manager classes for the entities.

--- SignalR.BusinessLayer/Abstract/IGenericService.cs
+++ SignalR.BusinessLayer/Abstract/IGenericService.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace SignalR.BusinessLayer.Abstract
 {
-    internal interface IGenericService
+    public interface IGenericService<T> where T : class
     {
+        void TAdd(T entity);
+        void TDelete(T entity);
+        void TUpdate(T entity);
+        T TGetByID(int id);
+        List<T> TGetListAll();
     }
 }
---
+++ SignalR.BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,46 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public void TAdd(Contact entity)
+        {
+            _contactDal.Add(entity);
+        }
+
+        public void TDelete(Contact entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Contact entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
