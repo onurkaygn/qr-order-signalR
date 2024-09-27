7b17f5be9fc9419646361a72d674f9be2e9039e3
= <onuralpkayginedu@gmail.com>
2024-09-27 14:36:05 +0300

    Recreated deleted entities after moving them from SignalRApi to SignalR.EntityLayer.

--- SignalR.EntityLayer/Entities/Discount.cs
+++ SignalR.EntityLayer/Entities/Discount.cs
@@ -2,5 +2,10 @@
 {
     public class Discount
     {
+        public int DiscountID { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
--- SignalR.EntityLayer/Entities/Product.cs
+++ SignalR.EntityLayer/Entities/Product.cs
@@ -2,5 +2,11 @@
 {
     public class Product
     {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
     }
 }
