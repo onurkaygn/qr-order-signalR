f05f3a313483333fecd16e39203f8f0682f73b9c
= <onuralpkayginedu@gmail.com>
2024-09-28 17:12:47 +0300

    Created controllers for 'Feature', 'Product', 'SocialMedia', and 'Testimonial' and registered them in Program.cs .

---
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.TestimonialDto;
+using SignalR.EntityLayer.DAL.Entities;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+        private readonly IMapper _mapper;
+
+        public TestimonialController(ITestimonialService testimonialService, IMapper mapper)
+        {
+            _testimonialService = testimonialService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var value = _mapper.Map<List<ResultTestimonialDto>>(_testimonialService.TGetListAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+        {
+            _testimonialService.TAdd(new Testimonial()
+            {
+               Title = createTestimonialDto.Title,
+               Comment = createTestimonialDto.Comment,
+               ImageUrl = createTestimonialDto.ImageUrl,
+               Name = createTestimonialDto.Name,
+               Status = createTestimonialDto.Status,
+            });
+            return Ok("Testimonial has been created successfully.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = _testimonialService.TGetByID(id);
+            _testimonialService.TDelete(value);
+            return Ok("Testimonial has been deleted successfully.");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            _testimonialService.TUpdate(new Testimonial()
+            {
+                TestimonialID = updateTestimonialDto.TestimonialID,
+                Title = updateTestimonialDto.Title,
+                Comment = updateTestimonialDto.Comment,
+                ImageUrl = updateTestimonialDto.ImageUrl,
+                Name = updateTestimonialDto.Name,
+                Status = updateTestimonialDto.Status,
+            });
+            return Ok("Testimonial has been updated successfully.");
+        }
+
+        [HttpGet("GetTestimonial")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var value = _testimonialService.TGetByID(id);
+            return Ok(value);
+
+        }
+    }
+}
