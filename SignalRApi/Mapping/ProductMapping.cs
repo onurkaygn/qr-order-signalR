b299aaadfc6278589d775a6a69cbb3d5f10c877d
= <onuralpkayginedu@gmail.com>
2024-09-28 16:13:13 +0300

    Created controller for 'About' and registered it in Program.cs.

--- SignalRApi/Mapping/ProductMapping.cs
+++ SignalRApi/Mapping/ProductMapping.cs
@@ -1,6 +1,17 @@
-﻿namespace SignalRApi.Mapping
+﻿using AutoMapper;
+using SignalR.DtoLayer.ProductDto;
+using SignalR.EntityLayer.DAL.Entities;
+
+namespace SignalRApi.Mapping
 {
-    public class ProductMapping
+    public class ProductMapping : Profile
     {
+        public ProductMapping()
+        {
+             CreateMap<Product,ResultProductDto>().ReverseMap();
+             CreateMap<Product,CreateProductDto>().ReverseMap();
+             CreateMap<Product,GetProductDto>().ReverseMap();
+             CreateMap<Product,UpdateProductDto>().ReverseMap();
+        }
     }
 }
